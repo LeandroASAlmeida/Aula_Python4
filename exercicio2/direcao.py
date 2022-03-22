ccdd885cc6a412432f0dab39aed63321a5887caf
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-03-22 18:38:38 -0300

    Update direcao.py
    correção exercicio

--- exercicio2/direcao.py
+++ exercicio2/direcao.py
@@ -1,26 +1,46 @@
-def inserir(lista, item):
-    if item in lista:
-        return print("Não é possível inserir itens duplicados, esse item já existe.")
-    else:
-        lista.append(item)
-        return print('Item adicionado com sucesso!')
 
-def alterar(lista, alterado, paraAlterar):
-    if alterado in lista:
-        for x in range(0, len(lista)):
-            if lista[x] == alterado:
-                lista[x] = paraAlterar    
-        return print('Valor alterado com sucesso!')
-    else:
-        return print(str(alterado) + ' não está cadastrado, impossível alterar!')
+op = 0
+def menu():
+    op = int(input('Operação desejada: \n' + 
+             '1- Listar      | 2- Cadastrar \n' + 
+             '3- Alterar     | 4- Excluir \n' + 
+             '           5- Sair\n'))
+    return op
 
-def visualizar(lista):
-    for x in range(0,len(lista)):
-        print(str(x+1) + ' - ' + lista[x])
+def listaVazia(obj):
+    return len(obj) == 0
 
-def excluir(lista, excluido):
-    if excluido in lista:
-        lista.remove(excluido)
-        return print('Item excluído com sucesso!')
+def listar(obj):
+    if not listaVazia(obj):
+        print(obj)
     else:
-        return print('Item inexistente, digite outro')
+        print('Lista vazia')
+
+def cadastrar(obj,valor):
+    if valor in obj:
+        print(valor + ' já está cadastrado')
+    else:
+        obj.append(valor)
+        print(valor + ' cadastrado com sucesso')
+
+def alterar(obj,valor): 
+    if not ( valor in obj):
+        print(valor + ' não está cadastrado na lista.')
+        return
+    for x in range(0,len(obj)):
+        if obj[x] == valor:
+            novo = input('Informe o novo valor para ' + valor + ': ')
+            if novo in obj:
+                print(novo + ' já está cadastrado.')
+            else:                
+                obj[x] = novo
+
+def excluir(obj,valor):
+    if len(obj) == 0:
+        print('A lista está vazia')
+    else:
+        if valor in obj:
+            obj.remove(valor)
+            print(valor + ' removido com sucesso!')
+        else:
+            print(valor + ' não existe na lista')
